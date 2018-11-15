aee4de6676a13172d0e8c4b82bd58318897e63c0
ouyunadmin <18321848354@163.com>
2018-11-15 09:29:27 +0800

    11.15早

--- lanliang/src/components/HelloWorld.vue
+++ lanliang/src/components/HelloWorld.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    
+      <h2>Essential Links</h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   },
-  created(){
-    this.$store.commit('addData','hello')
+  created() {
+    this.$store.commit("addData", "hello");
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
