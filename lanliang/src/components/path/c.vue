fc1c61a461e7e9a191e924c3e005159438560859
ouyunadmin <18321848354@163.com>
2019-04-24 17:26:25 +0800

    获取光标位置

--- lanliang/src/components/path/c.vue
+++ lanliang/src/components/path/c.vue
@@ -1,20 +1,43 @@
 <template>
-    <div>
-        C:{{c}}
-    </div>
+  <div>
+    C:{{c}}
+    <input ref="getDom" @keyup="onKey()" @focus="onFocus" value="sdklfjsldjf">
+    <button @click="onClick()">获取光标位置</button>
+  </div>
 </template>
 <script>
 export default {
-    data () {
-        return{
-            c: 'c路由'
-        }
-    },
-    created(){
-        console.log(this.$route.query.id)
+  data() {
+    return {
+      c: "c路由"
     }
+  },
+  created() {
+    // console.log(this.$route.query.id)
+  },
+  methods: {
+    onClick() {
+      var input = this.$refs.getDom
+      // var e = event.srcElement
+      // var r = e.createTextRange()
+      input.focus()
+      input.selectionStart = 3
+      input.selectionEnd = 5
+      // console.log(r)
+    },
+    onFocus() {
+      // var e = event.srcElement
+      // var r = e.createTextRange()
+      // console.log(e.setSelectionRange(1, 1))
+    },
+    onKey() {
+      // var input = this.$refs.getDom
+      // var e = event.srcElement
+      // var r = e.createTextRange()
+      // console.log(r)
+    }
+  }
 }
 </script>
 <style>
-
 </style>
